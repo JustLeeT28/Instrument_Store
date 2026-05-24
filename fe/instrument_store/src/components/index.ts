935b08d7c6908c892167f52612c3f09c18e0844a
Lê Anh Tuấn <22130310@st.hcmuaf.edu.vn>
2026-05-24 17:45:48 +0700

    feat(fe): add package and update css

---
+++ fe/instrument_store/src/components/index.ts
@@ -0,0 +1,4 @@
+export { Header } from './Header';
+export { Footer } from './Footer';
+export { ProductCard } from './ProductCard';
+export type { Product } from './ProductCard';
---
+++ fe/instrument_store/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { HomePage } from './HomePage';
+export { CartPage } from './CartPage';
+export { ProductDetailPage } from './ProductDetailPage';
+export { ProductsPage } from './ProductsPage';
