7d9886d5bcb98aa5fa725a6e79cfad53312d6cd3
Lê Anh Tuấn <22130310@st.hcmuaf.edu.vn>
2026-05-31 16:46:17 +0700

    Update application.properties

--- be/be-api/src/main/java/com/store/be_api/BeApiApplication.java
+++ be/be-api/src/main/java/com/store/be_api/BeApiApplication.java
@@ -1,7 +1,10 @@
 package com.store.be_api;
 
+import org.springframework.core.env.Environment;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvEntry;
@@ -21,4 +24,11 @@ public class BeApiApplication {
 		SpringApplication.run(BeApiApplication.class, args);
 	}
 
+    @Bean
+    CommandLineRunner printEnv(Environment env) {
+        return args -> {
+            System.out.println("URL = " + env.getProperty("spring.datasource.url"));
+            System.out.println("USER = " + env.getProperty("spring.datasource.username"));
+        };
+    }
 }
