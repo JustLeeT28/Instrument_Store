45b0e5af88690983a69f8e82f0588bd57e3fea49
Lê Anh Tuấn <22130310@st.hcmuaf.edu.vn>
2026-06-01 16:56:53 +0700

    Add register and login page

--- fe/instrument_store/src/App.tsx
+++ fe/instrument_store/src/App.tsx
@@ -1,25 +1,45 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Header, Footer } from './components';
-import { HomePage, CartPage, ProductDetailPage, ProductsPage, ProfilePage, FavoritePage } from './pages';
+import {
+  HomePage,
+  CartPage,
+  ProductDetailPage,
+  ProductsPage,
+  ProfilePage,
+  FavoritePage,
+  LoginPage,
+  RegisterPage,
+} from './pages';
 import './App.css';
+
+function AppContent() {
+  const location = useLocation();
+  const hideShell = ['/login', '/register'].includes(location.pathname);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {!hideShell && <Header />}
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/favorite" element={<FavoritePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </main>
+      {!hideShell && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/favorite" element={<FavoritePage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
